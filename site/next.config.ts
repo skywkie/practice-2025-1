ee5a0a080a70dd586b8d63382242a6b6d97ce03c
skywkie <dlunusin@gmail.com>
2026-05-12 18:22:21 +0300

    edit config

--- site/next.config.ts
+++ site/next.config.ts
@@ -6,8 +6,7 @@ const nextConfig: NextConfig = {
     unoptimized: true, // Обязательно для GitHub Pages
   },
   // Раскомментируйте, если сайт будет не в корне (например, username.github.io/repo-name)
-  basePath: "/site",
-  assetPrefix: "/site",
+  basePath: "/practice-2025-1",
 };
 
 export default nextConfig;
