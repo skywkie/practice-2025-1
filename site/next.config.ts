dad8718cd60f696fbb62ca08e71b3240d28969e1
skywkie <dlunusin@gmail.com>
2026-05-12 18:17:39 +0300

    edit github pages configuration

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Включает статический экспорт
+  output: "export", // Включает режим статического экспорта
   images: {
-    unoptimized: true, // Обязательно, так как next/image не работает при экспорте
+    unoptimized: true, // Обязательно для GitHub Pages
   },
+  // Раскомментируйте, если сайт будет не в корне (например, username.github.io/repo-name)
+  basePath: "/site",
+  assetPrefix: "/site",
 };
 
 export default nextConfig;
