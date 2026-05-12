5d857226101905e054bcaab67140de3ecfe520aa
skywkie <dlunusin@gmail.com>
2026-05-12 18:39:22 +0300

    refactor components

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/ui/header";
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/app/globals.css";
 
 export const metadata: Metadata = {
   title: "Видеотекст",
